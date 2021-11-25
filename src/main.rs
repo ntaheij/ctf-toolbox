30bbd989409d015d1d22a6f6f5190fc7038e546d
Noah Taheij <noah@taheij.nl>
2021-11-25 19:37:04 +0100

    Added basic shell

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use std::env;
+use std::io::*;
+use std::path::Path;
+use std::process::Command;
+
+mod crypt;
+mod colors;
+
+use colors::Colors::*;
+
+const PREFIX_COLOR: &str = CYAN.value();
+const PREFIX: &str = "toolkit>";
+
+fn main() {
+    spawn_shell();
+}
+
+fn spawn_shell() {
+    loop {
+        print!("{}{} {}", PREFIX_COLOR, PREFIX, RESET.value());
+        stdout().flush().unwrap();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        // everything after the first whitespace character
+        // is interpreted as args to the command
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let mut args = parts;
+
+        match command {
+            "cd" => {
+                // default to `/` as new directory if one was not provided
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    println!("{}", e);
+                }
+            }
+
+            "showcolors" | "colors" => {
+                colors::show_colors();
+            }
+
+            "exit" => return,
+
+            _ => {
+                let child = Command::new(command).args(args).spawn();
+                if let Err(e) = child {
+                    eprintln!("Unkown command -> {}", e);
+                } else {
+                    child.unwrap().wait().expect("child process error!");
+                }
+            }
+        }
+    }
+}
